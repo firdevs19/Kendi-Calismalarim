5c03c43d325f8beeff624cfb6c71a17cbf6d1ec3
firdevs19 <fiadhakeaf@gmail.com>
2023-05-21 23:11:24 +0200

    2023mayis20

--- src/GrupCalismalari/Mayis19.java
+++ src/GrupCalismalari/Mayis19.java
@@ -6,23 +6,11 @@ public class Mayis19 {
 
     public static void main(String[] args) {
 
-        String studentMap = "str";
 
-        // verilen soyisme sahip tum ogrencileri yazdirin
-        // verilen sube'deki tum ogrencileri yazdirin
-        // verilen sinif ve sube'deki tum ogrencileri yazdirin
 
-        /*
 
-        1- 101
-        2- 102
-        3- 103
-        4- 104
-        5- 105
-        6- 106
 
-         */
+
+        }
 
     }
-
-}
