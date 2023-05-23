39467bbdca424e7ffd08e25448eb0e879fcf93ec
firdevs19 <fiadhakeaf@gmail.com>
2023-05-23 16:32:18 +0200

    2023Mayis23

--- src/mayis22Budyler/WomensPower1.java
+++ src/mayis22Budyler/WomensPower1.java
@@ -12,34 +12,45 @@ Bir String’ de tekrarlanan karakterleri yazdırmak için kod yazınız.
 Örneğin; accessories ´ ces
  */
 
-
+int adet=0;
         for (int i = 120; i >= 11; i--) {
 
             if (i % 4 == 0 && i % 6 == 0) {
 
-                System.out.println(i + " ");
+                System.out.print(i + "(4/6) ");
+                adet++;
 
-
+            } else if (i % 4 == 0) {
+                System.out.print(i + "(4) ");
+                adet++;
+            } else if (i % 6 == 0) {
+                System.out.print(i + "(6) ");
+                adet++;
+            }
+            if (adet>0 && adet%10==0){
+                System.out.println("");
+                adet=0;
             }
 
         }
-
-        String str = "accessories";
+        System.out.println("");
+        String str = "accessoriesiessderfetrescerftoackt";
         System.out.print("Tekrarlanan karakterler: ");
 
         for (int i = 0; i < str.length(); i++) {
 
             char harf = str.charAt(i);
 
+
+            if (i>0 && str.substring(0,i).contains(String.valueOf(str.charAt(i)))){
+                continue;
+            }
+
             for (int j = i + 1; j < str.length(); j++) {
-
                 if (harf == str.charAt(j)) {
-
                     System.out.print(harf + " ");
                     break;
-
                 }
-
             }
         }
     }
